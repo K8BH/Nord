5f5e216c344d46198fc4f9adf9ed8206be13d6be
K8BH <kaustubhyt68@gmail.com>
2026-05-18 15:26:17 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,7 +1,19 @@
-const app = require("./ServerApp")
+const express = require('express')
+const path = require('path')
+const router = require('./Router')
 
+const app = express()
 const PORT = process.env.PORT || 3000
 
+app.use(express.json())
+app.use(express.static(__dirname))
+
+app.use('/api', router)
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'))
+})
+
 app.listen(PORT, () => {
-    console.log("Nord running on http://localhost:" + PORT)
+    console.log(`Nord running on http://localhost:${PORT}`)
 })
