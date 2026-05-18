23c71df823f2ef19a6053d1862067b8da8b1d1f4
K8BH <kaustubhyt68@gmail.com>
2026-05-18 19:09:56 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -6,12 +6,12 @@ const app = express()
 
 app.use(express.json())
 
-app.use(express.static(path.join(__dirname, "frontend")))
+app.use(express.static(__dirname))
 
 app.use("/api", router)
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "index.html"))
+    res.sendFile(path.join(__dirname, "index.html"))
 })
 
 module.exports = app
